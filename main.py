1600a5bbd4e1f0b5723c8ad8ac06cbab92ff932c
Heejung-KOREA <heejungs0304@gmail.com>
2024-04-08 16:14:00 +0900

    메모 수정/삭제 추가

--- main.py
+++ main.py
@@ -20,4 +20,20 @@ def create_memo(memo:Memo): #값을 Memo로 받음
 def read_memo():
     return memos
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id == req_memo.id:  #기존 메모의 id랑 전달 받은 id값이 같을 때,
+            memo.content = req_memo.content
+            return '성공했습니다'
+        return '실패'
+    
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+        for index, memo in enumerate(memos):    #enumerate -> 배열의 인덱스 값과 값을 같이 뽑아주는 함수
+            if memo.id == memo.id:  #기존 메모의 id랑 전달 받은 id값이 같을 때,
+                memos.pop(index)
+                return '성공했습니다'
+        return '실패'
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,9 +1,54 @@
+//작성했던 메모 수정하기
+async function editMemo(event) {
+    const id = event.target.dataset.id; //id값을 가져옴
+    const editInput = prompt('수정할 값을 입력하세요');
+    //수정한 값 서버 업데이트 요청
+    const res = await fetch('/memos/${id}', {
+        method: "PUT",  //값 수정 메소드
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            id,     //id: id인 경우 생략해도 됨
+            content: editInput,
+        }),
+    });
+
+    readMemo();
+}
+
+//메모 삭제
+async function deleteMemo(event) {
+    const id = event.target.dataset.id; //id값을 가져옴
+    //삭제한 값 서버 업데이트 요청
+    const res = await fetch('/memos/${id}', {
+        method: "DELETE",  //값 삭제 메소드
+    });
+
+    readMemo();
+}
+
 //가져온 메모 보여주기
 function displayMemo(memo) {
     const ul = document.querySelector("#memo-ul");
     const li = document.createElement("li");    //li 태그 생성
     li.innerHTML = `[id:${memo.id}] ${memo.content}`;
+
+    //수정하기
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "수정하기";
+    editBtn.addEventListener("click", editMemo);
+    editBtn.dataset.id = memo.id;
+
+    //삭제하기
+    const delbtn = document.createElement("button");
+    delbtn.innerText = "삭제";
+    delbtn.addEventListener("click", deleteMemo);
+    delbtn.dataset.id = memo.id;
+
     ul.appendChild(li); //ul에 추가한 li 넣기
+    li.appendChild(editBtn)  //추가한 button  넣기
+    li.appendChild(delbtn)
 }
 
 //작성했던 메모 가져오기
